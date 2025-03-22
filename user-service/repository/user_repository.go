af690f39601f5785a8b31996076fc8f8fb84ed84
Muxsin <muxsin.shokirov@gmail.com>
2025-03-22 22:45:55 +0700

    Implemented func to find user by id

--- user-service/repository/user_repository.go
+++ user-service/repository/user_repository.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"encoding/json"
+	"errors"
 	"fmt"
 
 	"github.com/Kodnavis/face2face-backend/user-service/model"
@@ -25,4 +26,24 @@ func (u *UserRepo) Insert(ctx context.Context, user model.User) error {
 	}
 
 	return nil
+}
+
+var ErrNotExist = errors.New("user does not exist")
+
+func (u *UserRepo) FindByID(ctx context.Context, id uint64) (model.User, error) {
+	var data string
+	err := u.Db.QueryRowContext(ctx, "SELECT data from users WHERE $1", id).Scan(&data)
+	if errors.Is(err, sql.ErrNoRows) {
+		return model.User{}, ErrNotExist
+	} else if err != nil {
+		return model.User{}, fmt.Errorf("failed to decode order json: %w", err)
+	}
+
+	var user model.User
+	err = json.Unmarshal([]byte(data), &user)
+	if err != nil {
+		return model.User{}, fmt.Errorf("failed to decode user json: %w", err)
+	}
+
+	return user, nil
 }
